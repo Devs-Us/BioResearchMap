a8ecbbee0ddb54afa9327744c8176a1c01441c32
amsyarasyiq <amsyarasyiq@gmail.com>
2021-06-26 11:36:34 +0800

    some unimportant commit

---
+++ LevelImposter/Harmony/Patches/TextPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using LevelImposter.Map;
+using LevelImposter.MinimapGen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LevelImposter.Harmony.Patches
+{
+    [HarmonyPatch(typeof(TranslationController), nameof(TranslationController.GetString), typeof(SystemTypes))]
+    public static class TextPatch
+    {
+        public static bool Prefix(SystemTypes room, ref string __result)
+        {
+            if (TextHandler.Contains(room))
+            {
+                __result = TextHandler.Get(room);
+                return false;
+            }
+            return true;
+        }
+    }
+}
